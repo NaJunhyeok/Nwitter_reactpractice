178fcd3cb183a32e2f5e334aca30dfd2b0fa5485
NaJunhyeok <njs4922@gmail.com>
2024-03-20 00:55:01 +0900

    first

---
+++ nwitter/src/App.tsx
@@ -0,0 +1,54 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element : <Layout />,
+  children: [
+    {
+      path :"",
+      element : <Home />,
+    },
+    {
+      path : "profile",
+      element : <Profile />,
+    }
+  ]
+  },
+  {
+    path:"/login",
+    element :<Login />
+  },
+  {
+    path:"/create-account",
+    element:<CreateAccount />
+  }
+])
+const GlobalStytles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizign : border-box;
+  }
+  body{
+    background-color : black;
+    color:white;
+    font-family:system-ui,
+  }
+`;
+
+function App() {
+
+  return(
+  <>
+    <GlobalStytles />
+    <RouterProvider router={router}/>
+  </>);
+}
+export default App;
